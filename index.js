5a14fe29d740d0201ffef5cc824447f94427334b
Ibohim <Ibohim>
2024-06-10 16:25:11 +0500

    if-else

---
+++ index.js
@@ -0,0 +1,40 @@
+let name = prompt(" Ismingizni kiriting")
+    console.log("ismim " + name);
+let age = prompt(" yoshingizni kiriting")
+    console.log("yoshim " + age + "yoshda")
+let misol1 = prompt("birinchi misol: 9 + 9 = ?")
+if (misol1==18){
+    console.log("javobingiz tog'ri " + misol1)
+}
+else{
+    console.log("javobingiz xato " + misol1+" to'gri javob 18")
+}
+let misol2 = prompt("ikkinchi misol: 9 - 8 = ?")
+if (misol2==1){
+    console.log("javobingiz tog'ri " + misol2)
+}
+else{
+    console.log("javobingiz xato " + misol2+" to'gri javob 1")
+}
+let misol3 = prompt("uchinchi misol: 9 * 9 = ?")
+if (misol3==81){
+    console.log("javobingiz tog'ri " + misol3)
+}
+else{
+    console.log("javobingiz xato " + misol3+" to'gri javob 81")
+}
+let misol4 = prompt("to'rtinchi misol: 9 / 9 = ?")
+if (misol4==1){
+    console.log("javobingiz tog'ri " + misol4)
+}
+else{
+    console.log("javobingiz xato " + misol4 +" to'gri javob 1")
+}
+let misol5 = prompt("beshinchi misol: 9 / 2 = ?")
+if (misol5==4.5){
+    console.log("javobingiz tog'ri " + misol5)
+}
+else{
+    console.log("javobingiz xato " + misol5+" to'gri javob 4.5")
+}
+alert("natijani ko'rish uchun consolega o'ting")
